82ff07db05f83449c4fbf837a7cd1037ae91e0dd
Ryan Burton <rryanburton@gmail.com>
2020-12-10 18:54:26 -0500

    add signup and login components

---
+++ frontend/src/components/dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+// frontend/src/componenets/dashboard/Dashboard.js
+
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>Dashboard</h1>
+      </Container>
+    );
+  }
+}
+
+export default Dashboard;
